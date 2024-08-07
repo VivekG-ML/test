effb291e77c4a90e622f703f10162c24c069c230
VivekG-ML <vivekgandhele01@gmail.com>
2024-08-07 07:16:09 +0530

    fries

--- hungry.py
+++ hungry.py
@@ -3,5 +3,6 @@ hungry = input("are you hungry?")
 if hungry == "yes":
     print("Let's have a pizza then!")
     print("And let's also have a burger!")
+    print("And let's get fries as well!")
 else:
     print("Okay go back to your work then!")
