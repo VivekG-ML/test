3eb91b0f69c8ac89b3623f03ebfc381bee261481
VivekG-ML <vivekgandhele01@gmail.com>
2024-08-10 06:09:40 +0530

    added y

--- hungry.py
+++ hungry.py
@@ -1,6 +1,6 @@
 print("I'm really hungry!")
 hungry = input("are you hungry?")
-if hungry == "yes":
+if hungry == "yes" and hungry == "y":
     print("Let's have a pizza then!")
     print("And let's also have a burger!")
     print("And let's get fries as well!")
